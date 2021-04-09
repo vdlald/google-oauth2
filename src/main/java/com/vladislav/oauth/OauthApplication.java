3be6061a54881849fc11c39f3bf23f890151b49f
Vladislav Golubinov <vlad.vdlald@gmail.com>
2021-04-09 19:11:00 +0300

    init

---
+++ src/main/java/com/vladislav/oauth/OauthApplication.java
@@ -0,0 +1,13 @@
+package com.vladislav.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OauthApplication.class, args);
+  }
+
+}
